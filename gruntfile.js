e0e55a5bae372a798754ca8812b7a8308a2dfdf8
Rebecca Cremona <rebecca_cremona@harvard.edu>
2015-10-08 16:45:30 -0400

    Add html validation to grunt test task.

--- gruntfile.js
+++ gruntfile.js
@@ -284,7 +284,7 @@ module.exports = function (grunt) {
     grunt.registerTask('build', ['copy:bootstrapCustom','exec:jekyllBuild','concat','sass']);
     grunt.registerTask('rebuild', ['exec:jekyllClear','build']);
 
-    grunt.registerTask('test', ['bootlint', 'linkChecker:dev', 'exec:findRelics', 'exec:testRedirects',
+    grunt.registerTask('test', ['htmllint','bootlint', 'linkChecker:dev', 'exec:findRelics', 'exec:testRedirects',
                        'exec:checkBaseurl']);
     grunt.registerTask('polish', ['exec:findNotes']);
 
