94bde302bb212bc9f9cbba3e51dda811a344194f
Rebecca Cremona <rebecca_cremona@harvard.edu>
2015-10-23 13:53:29 -0400

    Remove spurious comment from test.

--- tests/check_baseurl/check_baseurl.sh
+++ tests/check_baseurl/check_baseurl.sh
@@ -3,7 +3,6 @@ mkdir tmp
 
 echo "Looking for relative links that don't use {{site.baseurl}}..."
 grep -Enor --include=*.md "\[.*\]\(/.*\)" ../../app | grep -Evf exclude_lines.txt >> tmp/errors.txt
-echo "Looking for braces (relics of liquid tags, kramdown)..."
 grep -Enor --include=*.html "href=\"/.*\"" ../../app | grep -Ev "site.baseurl" | grep -Evf exclude_lines.txt >> tmp/errors.txt
 echo ""
 
