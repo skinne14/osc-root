1f7f0552c536c277041597c26125473ca1ddb86f
Rebecca Cremona <rebecca_cremona@harvard.edu>
2015-11-06 15:05:32 -0500

    Reintroduce curly quotes within legacy urls (AH!), account for in test.

--- tests/find_curly_quotes/find_curly_quotes.sh
+++ tests/find_curly_quotes/find_curly_quotes.sh
@@ -2,9 +2,9 @@
 mkdir tmp
 
 echo "Looking for curly quotes and apostrophes..."
-grep -Enor --include=*.html ".{0,15}[“”’‘].{0,15}" ../../app/ >> tmp/errors.txt
-grep -Enor --include=*.md ".{0,15}[“”’‘].{0,15}" ../../app/ >> tmp/errors.txt
-grep -Enor --include=*.txt ".{0,15}[“”’‘].{0,15}" ../../app/ >> tmp/errors.txt
+grep -Enor --include=*.html ".{0,15}[“”’‘].{0,15}" ../../app/ | grep -Evf exclude_curls.txt >> tmp/errors.txt 
+grep -Enor --include=*.md ".{0,15}[“”’‘].{0,15}" ../../app/ | grep -Evf exclude_curls.txt >> tmp/errors.txt
+grep -Enor --include=*.txt ".{0,15}[“”’‘].{0,15}" ../../app/ | grep -Evf exclude_curls.txt >> tmp/errors.txt
 
 echo ""
 
